32176938c9c0b7a6c0a461e74dab8fd457fccbb1
David Garzon Velez <dgv241198@gmail.com>
2022-07-15 09:11:31 -0500

    feat: Add types for repositories

--- src/domain/repositories/offer.repository.ts
+++ src/domain/repositories/offer.repository.ts
@@ -1,9 +1,10 @@
 import { Offer } from '../entities/offer.entity';
+import { UpdateResult } from './types/userRepository.types';
 
 export interface OfferRepository {
 	getByName(name: string): Promise<Offer | undefined>;
 	getById(id: string): Promise<Offer | undefined>;
 	create(newOffer: Offer, transaction?: any): Promise<Offer>;
 	get(transaction?: any): Promise<Offer[]>;
-	update(id: string, offer: object): Promise<{ affectedCount: number }>;
+	update(id: string, offer: object): Promise<UpdateResult>;
 }
---
+++ src/domain/repositories/types/userRepository.types.ts
@@ -0,0 +1,1 @@
+export type UpdateResult = { affectedCount: number }
